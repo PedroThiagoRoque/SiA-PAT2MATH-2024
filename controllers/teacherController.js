ae6ab969ef7e7089d96f6c35fc99d768655d8c93
PedroThiagoRoque <pedronmroque@gmail.com>
2024-07-06 15:38:57 -0300

    Gerenciamento de problemas pelo professor
    crud de Meus problemas e visualização dos problemas do banco de problemas

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,9 +1,19 @@
-const Class = require('../models/Class');
+const mongoose = require('mongoose');
 const Problem = require('../models/Problem');
+const Class = require('../models/Class');
 const Teacher = require('../models/Teacher');
 const Student = require('../models/Student');
-const mongoose = require('mongoose');
 
+// Função utilitária para validar ObjectIds
+const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
+
+// Ordem personalizada das dificuldades
+const difficultyOrder = ['easy', 'medium', 'hard'];
+
+// Função de comparação para ordenar por dificuldade
+const compareDifficulty = (a, b) => {
+  return difficultyOrder.indexOf(a.difficulty) - difficultyOrder.indexOf(b.difficulty);
+};
 
 // Criar uma nova turma
 const createClass = async (req, res) => {
@@ -17,23 +27,10 @@ const createClass = async (req, res) => {
       return res.status(400).json({ msg: 'Professor não encontrado' });
     }
 
-    const problemsArray = problems ? problems.split(',').map(id => id.trim()).filter(id => id) : [];
-
-    // Verificar se todos os IDs de problemas são válidos
-    for (const problemId of problemsArray) {
-      const problemExists = await Problem.findById(problemId);
-      if (!problemExists) {
-        return res.render('teacher/edit-class', {
-          classItem: { _id: req.params.id, name, problems: problemsArray.join(', ') },
-          errorMsg: `O problema com ID ${problemId} não existe.`
-        });
-      }
-    }
-
     const newClass = new Class({
       name,
       teacher: teacher._id,
-      problems: problemsArray
+      problems: problems ? problems.split(',').map(id => id.trim()) : []
     });
 
     await newClass.save();
@@ -106,19 +103,6 @@ const listClasses = async (req, res) => {
   }
 };
 
-// Excluir turma
-const deleteClass = async (req, res) => {
-  const classId = req.params.id;
-
-  try {
-    await Class.findByIdAndDelete(classId);
-    res.redirect('/api/teacher/dashboard');
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Erro no servidor');
-  }
-};
-
 // Renderizar a página de edição da turma
 const renderEditClassPage = async (req, res) => {
   const classId = req.params.id;
@@ -130,16 +114,12 @@ const renderEditClassPage = async (req, res) => {
       return res.status(400).json({ msg: 'Turma não encontrada' });
     }
 
-    res.render('teacher/edit-class', { classItem, errorMsg: null });
+    res.render('teacher/edit-class', { classItem });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Erro no servidor');
   }
 };
-
-// Editar turma
-// Função utilitária para validar ObjectIds
-const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
 
 // Editar turma
 const editClass = async (req, res) => {
@@ -175,11 +155,114 @@ const editClass = async (req, res) => {
   }
 };
 
+// Excluir turma
+const deleteClass = async (req, res) => {
+  const classId = req.params.id;
+
+  try {
+    await Class.findByIdAndDelete(classId);
+    res.redirect('/api/teacher/dashboard');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
+// Listar problemas
+const listProblems = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    // Buscar o ID do professor usando o ID do usuário
+    const teacher = await Teacher.findOne({ userId: userId });
+    if (!teacher) {
+      return res.status(400).json({ msg: 'Professor não encontrado' });
+    }
+
+    const teacherId = teacher._id;
+
+    const myProblems = await Problem.find({ createdBy: teacherId }).exec();
+    const otherProblems = await Problem.find({ createdBy: { $ne: teacherId } }).exec();
+
+    // Ordenar problemas por dificuldade usando a função de comparação personalizada
+    myProblems.sort(compareDifficulty);
+    otherProblems.sort(compareDifficulty);
+
+    res.render('teacher/list-problems', { myProblems, otherProblems });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
+
+
+// Renderizar a página de edição do problema
+const renderEditProblemPage = async (req, res) => {
+  const problemId = req.params.id;
+
+  try {
+    const problem = await Problem.findById(problemId);
+
+    if (!problem) {
+      return res.status(400).json({ msg: 'Problema não encontrado' });
+    }
+
+    res.render('teacher/edit-problem', { problem });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
+// Editar problema
+const editProblem = async (req, res) => {
+  const problemId = req.params.id;
+  const { title, statement, equation, result, difficulty } = req.body;
+
+  try {
+    const problem = await Problem.findById(problemId);
+
+    if (!problem) {
+      return res.status(400).json({ msg: 'Problema não encontrado' });
+    }
+
+    problem.title = title;
+    problem.statement = statement;
+    problem.equation = equation;
+    problem.result = result;
+    problem.difficulty = difficulty;
+    await problem.save();
+
+    res.redirect('/api/teacher/list-problems');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
+// Excluir problema
+const deleteProblem = async (req, res) => {
+  const problemId = req.params.id;
+
+  try {
+    await Problem.findByIdAndDelete(problemId);
+    res.redirect('/api/teacher/list-problems');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
 module.exports = {
   createClass,
   createProblem,
   listClasses,
   deleteClass,
   renderEditClassPage,
-  editClass
+  editClass,
+  listProblems,
+  renderEditProblemPage,
+  editProblem,
+  deleteProblem
 };
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createClass, createProblem, listClasses, deleteClass, renderEditClassPage, editClass } = require('../controllers/teacherController');
+const { createClass, createProblem, listClasses, deleteClass, renderEditClassPage, editClass, listProblems, renderEditProblemPage, editProblem, deleteProblem } = require('../controllers/teacherController');
 const auth = require('../middleware/auth');
 
 // Rotas para Turmas
@@ -11,6 +11,12 @@ router.post('/create-class', auth, createClass);
 router.post('/create-problem', auth, createProblem);
 router.get('/edit-class/:id', auth, renderEditClassPage);
 router.post('/edit-class/:id', auth, editClass);
-router.get('/delete-class/:id', auth, deleteClass);
+router.post('/delete-class/:id', auth, deleteClass);
+
+// Rotas para Problemas
+router.get('/list-problems', auth, listProblems);
+router.get('/edit-problem/:id', auth, renderEditProblemPage);
+router.post('/edit-problem/:id', auth, editProblem);
+router.post('/delete-problem/:id', auth, deleteProblem);
 
 module.exports = router;
