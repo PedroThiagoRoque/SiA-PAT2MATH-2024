e4c3d51f6d27cd7bd223995f30330ee9f5c7a932
PedroThiagoRoque <pedronmroque@gmail.com>
2024-07-11 00:27:52 -0300

    Revert "Módulo Expert"
    This reverts commit 49cd8ec495fd7f437e4a55772178b4f3cb4f38f7.

--- controllers/expertController.js
+++
@@ -1,8 +0,0 @@
-const StepAnalyzer = require('./stepAnalyzer');
-
-exports.evaluateStep = (req, res) => {
-  const { equation, userStep } = req.body;
-  const isValid = StepAnalyzer.analyzeStep(equation, userStep);
-  const feedback = StepAnalyzer.getFeedback(isValid);
-  res.json({ isValid, feedback });
-};
